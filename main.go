a037a123e86a111ea6164b800b0508054ede7742
Muhammad Arief <mgi.official1@gmail.com>
2025-11-19 07:13:14 +0700

    feat(config): load configuration and initialize app dependencies

--- main.go
+++ main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"fmt"
+	"hr-leave-request/config"
 	"hr-leave-request/injector"
 
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	// load configuration and initialize dependencies using wire
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		logrus.Fatalf("failed to load config: %v", err)
+	}
+
 	// Initialize app with wire
 	app, err := injector.InitializeApp()
 	if err != nil {
@@ -15,7 +22,7 @@ func main() {
 	}
 
 	// Start server
-	port := 3000
+	port := cfg.AppConfig.Port
 	logrus.Infof("Starting HR Leave Request API on port %d", port)
 	if err := app.Listen(fmt.Sprintf(":%d", port)); err != nil {
 		logrus.Fatalf("failed to start server: %v", err)
